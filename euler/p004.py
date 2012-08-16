d255a1b9d245f029c8f6cf00d730558148253cb0
Zsolt Cserna <cserna.zsolt@gmail.com>
2012-08-16 23:01:08 +0200

    Minor code improvements.

--- euler/p004.py
+++ euler/p004.py
@@ -9,7 +9,7 @@ for op1, op2 in itertools.combinations_with_replacement(xrange(999, 0, -1), 2):
     
     if len(res) % 2 == 0:
         left = res[:len(res)/2]
-        right = res[:0-(len(res)/2+1):-1]
+        right = res[:-1-len(res)/2:-1]
         if left == right:
             results.append(int(res))
             
