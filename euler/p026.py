4d997dac5d081500a8c90a7ce15a12d503e8ef8a
Cserna Zsolt <cserna.zsolt@gmail.com>
2012-10-06 11:19:43 +0200

    Next iteration..

--- euler/p026.py
+++ euler/p026.py
@@ -1,5 +1,5 @@
 
-
+import largest
 from decimal import Decimal, getcontext
 
 getcontext().prec = 1000
@@ -8,26 +8,25 @@ import re
 
 sol_l = 0
 sol = None
+idx = None
 
 for i in xrange(1, 1001):
-#    print i
+    print i, sol
     result = one / Decimal(i)
     result_s = str(result)[2:-1]
-#    print i, result_s 
     if not result_s:
         continue
         
-    for idx in xrange(len(result_s)/2, 2, -1):
-        if result_s[:idx] == result_s[idx:idx*2]:
-            tmp = result_s[:idx]
-            break
-            
-            
-    print i, len(tmp)
-    if i == 997:
-        print result_s
+    tmp = largest.find_largest_cycle(result_s)
     if len(tmp)>sol_l:
         sol_l = len(tmp)
-        sol = (result, tmp, sol_l, i)
-
+        sol = tmp
+        idx = i
+        
+    
+        
+    
+print idx
 print sol
+print sol_l
+
