c4a5a33432b5a082f2433065dedb677bd39e44cd
Cserna Zsolt <cserna.zsolt@gmail.com>
2012-12-24 01:32:11 +0100

    Solution for problem 44.

---
+++ euler/p044.py
@@ -0,0 +1,24 @@
+
+n = 2
+numbers = [1]
+n_set = set(numbers)
+
+import math
+
+def is_penta(x):
+    
+    
+    return (1+math.sqrt(1-12*(-2*x))) % 6 == 0
+
+found = False
+while not found:
+    pn = n*(3*n-1)/2
+    for prev in numbers:
+        if (pn-prev in n_set or abs(prev-pn) in n_set) and is_penta(pn+prev):
+            print pn, prev, pn-prev
+            found = True
+    else:
+        numbers.append(pn)
+        n_set.add(pn)
+        n = n + 1
+    
