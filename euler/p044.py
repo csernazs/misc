11e5ea9baf598847edf49fbab294b5aa2539de2e
Cserna Zsolt <cserna.zsolt@gmail.com>
2012-12-24 01:38:55 +0100

    Code cleanup.

--- euler/p044.py
+++ euler/p044.py
@@ -6,8 +6,6 @@ n_set = set(numbers)
 import math
 
 def is_penta(x):
-    
-    
     return (1+math.sqrt(1-12*(-2*x))) % 6 == 0
 
 found = False
@@ -17,6 +15,7 @@ while not found:
         if (pn-prev in n_set or abs(prev-pn) in n_set) and is_penta(pn+prev):
             print pn, prev, pn-prev
             found = True
+            break
     else:
         numbers.append(pn)
         n_set.add(pn)
