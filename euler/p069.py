ab2d938e18a5a882bb3451f48477b8dd0ed15879
zsolt <cserna.zsolt@gmail.com>
2013-12-22 17:25:11 +0100

    Solution for problem 69 in python.

--- euler/p069.py
+++ euler/p069.py
@@ -1,41 +1,34 @@
 
 from __future__ import division
 
-cache = {}
-
 def gcd(a,b):
-    if (a,b) in cache:
-        return cache[(a,b)]
-    else:
-      params = (a,b)
+    while b != 0:
+        a, b = b, a % b
+    return a
           
-    while a != b:
-        if a>b:
-            a = a - b
-        else:
-            b = b - a
-    
-    cache[params] = a
-    return a      
-      
 def phi(n):
     cnt = 1
     for i in xrange(2, n):
-        if n>i:
-          res = gcd(n, i)
-        elif n<i:
-          res = gcd(i, n)
-        else:
-          res = i
-        
+        res = gcd(n, i)
         if res == 1:
             cnt += 1
-            
     return cnt
             
 
-#print phi(9)
+maxres = 0
+primes = [11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+primes_i = iter(primes)
 
-for i in xrange(2, 1000001):
+i = 210
+maxres = 0
+
+while True:
+  i = i * primes_i.next()
+  if i > 1000000:
+    break
   res = phi(i)
-  print i, res, i/res
+  divres = i / res
+  if divres>maxres:
+    print i, res, divres
+    maxres = divres
+  
