74b6aefeaff48d379dc148c5a0e95d9ef0757d99
Cserna Zsolt <cserna.zsolt@gmail.com>
2016-08-07 13:19:22 +0200

    shining.py: added floating point matrix

--- microbit/shining.py
+++ microbit/shining.py
@@ -2,22 +2,37 @@
 from microbit import *
 import random
 
+random_range = [x/100.0 for x in range(20, 151)]
+
 matrix = []
 for rowidx in range(5):
-    matrix.append([random.choice([-1, 1]) for i in range(5)])
+    matrix.append([random.choice(random_range) for i in range(5)])
 
+pixels = []
 for rowidx in range(5):
+    row = []
+    pixels.append(row)
     for colidx in range(5):
-        display.set_pixel(rowidx, colidx, random.randint(0, 9))
-        
+        value = random.randint(0, 9)
+        display.set_pixel(rowidx, colidx, value)
+        row.append(value)
+
 while True:
     for rowidx in range(5):
         for colidx in range(5):
-            pixel = display.get_pixel(rowidx, colidx)
+            pixel = pixels[rowidx][colidx]
             new_pixel = pixel+matrix[rowidx][colidx]
             if new_pixel>9 or new_pixel<0:
                 matrix[rowidx][colidx] = 0 - matrix[rowidx][colidx]
                 new_pixel = pixel + matrix[rowidx][colidx]
-                
-            display.set_pixel(rowidx, colidx, new_pixel)
+
+            pixels[rowidx][colidx] = new_pixel
+            display.set_pixel(rowidx, colidx, int(new_pixel))
     sleep(50)
+    if button_a.is_():
+        for rowidx in range(5):
+            for colidx in range(5):
+                pixels[rowidx][colidx] = 9
+                matrix[rowidx][colidx] = random.choice(random_range)
+                if matrix[rowidx][colidx]>0:
+                    matrix[rowidx][colidx] = 0 - matrix[rowidx][colidx]
