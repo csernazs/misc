465e2a948ed45ea3d82cb78a8ba8ae0a41ad390a
Cserna Zsolt <cserna.zsolt@gmail.com>
2024-12-24 14:56:16 +0100

    aoc2024: solution for day 10

--- rust/aoc2024/src/bin/aoc_10.rs
+++ rust/aoc2024/src/bin/aoc_10.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::{HashMap, HashSet, VecDeque},
     str::FromStr,
+    time::Instant,
 };
 
 use clap::{command, Parser};
@@ -93,8 +94,6 @@ fn solve1(input: &Input) -> usize {
         queue.push_back(Item { origin: pos, pos });
     }
 
-    dbg!(&origins);
-
     while !queue.is_empty() {
         let item = queue.pop_front().unwrap();
 
@@ -114,25 +113,67 @@ fn solve1(input: &Input) -> usize {
         }
     }
 
-    dbg!(&origins);
-
     find(&input.map, 0)
         .map(|pos| origins.get(&pos).unwrap().len())
         .sum()
 }
+
+fn solve2(input: &Input) -> usize {
+    let mut counts: HashMap<(usize, usize), usize> = HashMap::new();
+
+    let mut queue: VecDeque<(usize, usize)> = VecDeque::new();
+
+    let (height, width) = input.map.dim();
+
+    for row in 0..height {
+        for col in 0..width {
+            counts.insert((row, col), 0);
+        }
+    }
+
+    for pos in find(&input.map, 9) {
+        counts.insert(pos, 1);
+        queue.push_back(pos);
+    }
+
+    while !queue.is_empty() {
+        let pos = queue.pop_front().unwrap();
+
+        let value = input.map[pos];
+        if value == 0 {
+            continue;
+        }
+        for neigh_pos in get_neigh(&input.map, pos.0, pos.1) {
+            let neigh_value = input.map[neigh_pos];
+            if neigh_value == value - 1 {
+                counts.entry(neigh_pos).and_modify(|x| *x += 1);
+                queue.push_back(neigh_pos);
+            }
+        }
+    }
+
+    find(&input.map, 0)
+        .map(|pos| counts.get(&pos).unwrap())
+        .sum()
+}
+
 fn main() {
     let cli = Cli::parse();
     let data = std::fs::read_to_string(&cli.input).expect("Unable to open file");
 
+    let start = Instant::now();
     let input: Input = data.parse().unwrap();
     println!("{}", solve1(&input));
+    println!("{}", solve2(&input));
+    let duration = start.elapsed();
+    println!("Elapsed time: {} μs", duration.as_micros());
 }
 
 #[cfg(test)]
 mod tests {
     use ndarray::array;
 
-    use crate::{solve1, Input};
+    use crate::{solve1, solve2, Input};
 
     fn get_sample_input() -> Input {
         Input::new(array![
@@ -163,5 +204,11 @@ mod tests {
     fn test_solve1() {
         let input = get_sample_input();
         assert_eq!(solve1(&input), 36);
+    }
+
+    #[test]
+    fn test_solve2() {
+        let input = get_sample_input();
+        assert_eq!(solve2(&input), 81);
     }
 }
